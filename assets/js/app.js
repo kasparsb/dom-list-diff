99099fce1bcba6f272eaa2e6e4f387ddf31314dd
Kaspars Bulins <kasparsb@hotmail.com>
2021-02-25 19:42:08 +0000

    First

---
+++ assets/js/app.js
@@ -0,0 +1,50 @@
+import append from 'dom-helpers/src/append';
+import remove from 'dom-helpers/src/remove';
+import jsx from 'dom-helpers/src/jsx';
+import q from 'dom-helpers/src/q';
+import clickp from 'dom-helpers/src/event/clickp';
+
+import getNewData from './getNewData';
+
+let currentData = [];
+
+let container = q('.cards');
+
+
+function diff(newData, currentData) {
+    let newItems = [];
+
+    // Visiem pieliekam pazīmi nav atrats
+    currentData.map(d => d._found = false);
+
+    newData.forEach(data => {
+        let found = currentData.find(item => item.id == data.id);
+
+        // Atrats
+        if (found) {
+            // Liekam pazīmi, ka atrasts
+            found._found = true;
+
+            // Pārceļam DOM elementa referenci uz jauno datu masīvu
+            data.el = found.el;
+        }
+        // Nav atrasts, jāliek iekšā
+        else {
+            data.el = append(container, <div class="card">{data.text}</div>)
+        }
+    })
+
+    // Dzēšam pazudušos
+    currentData.filter(d => !d._found).map(d => remove(d.el))
+
+    return newData;
+}
+
+function loadData() {
+    currentData = diff(getNewData(), currentData);
+}
+
+clickp('[name=refresh]', loadData)
+
+
+loadData();
---
+++ assets/js/getNewData.js
@@ -0,0 +1,18 @@
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getNewData() {
+
+    let start = random(1, 10);
+    let end = random(start+10, 30);
+
+    let r = [];
+    for (let i = start; i < end; i++) {
+        r.push({id: i, text: i+' row'})
+    }
+
+    return r
+}
+
+export default getNewData
